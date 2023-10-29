6fd2f6094e1e3bf8097ba6744e67aaeea38615df
Born2bATM <kachamaslove25@gmail.com>
2023-10-29 10:06:54 +0700

    Apply new calculated rule

--- calLowestPriceWithAlreadyFilledPlayers.py
+++ calLowestPriceWithAlreadyFilledPlayers.py
@@ -4,20 +4,32 @@
 # K Dice Combinations
 # Using list comprehension + product()
 from itertools import product
+import math
  
 # initializing K
-K = 11
-goalStats = 82.5
-alreadyFilled = list([83,80,81,79,82])
+playersNum = 11
+goalStats = 85
+alreadyFilled = list([84,83,84,84,82,85])
 totalStats = 0
-remainingK = 11-len(alreadyFilled)
- 
-for i in range(0,len(alreadyFilled)):
-    totalStats+=alreadyFilled[i]
+remainingPlayersNum = playersNum-len(alreadyFilled)
 
+def totalStats(listOfStats):
+    totalStats=0
+    for i in range(0,len(listOfStats)):
+        totalStats+=listOfStats[i]
+    return totalStats
 
-stats = [list([82,83,84,85]) for _ in range(remainingK)]
-price = [list([600,900,2100,6250]) for _ in range(remainingK)]
+def calStat(listOfStats):
+    totalStat = totalStats(listOfStats)
+    AvgStat = totalStat/playersNum
+    totalBoostPoints=0
+    for i in range(0,len(listOfStats)):
+        if(listOfStats[i]>AvgStat):
+            totalBoostPoints += (listOfStats[i]-AvgStat)
+    return math.floor(round(totalStat+totalBoostPoints)/11)
+
+stats = [list([82,83,84,85,86]) for _ in range(remainingPlayersNum)]
+price = [list([600,900,2100,6250,11000]) for _ in range(remainingPlayersNum)]
 
 # using product() to get Combinations
 probStats = list(product(*stats))
@@ -29,22 +41,21 @@ caseAvgStats = list()
 
 for i in range(0,len(probPrice)):
     p = 0
-    s = 0
-    for j in range(0,remainingK):
+    for j in range(0,remainingPlayersNum):
         p+=probPrice[i][j]
-        s+=probStats[i][j]
-    if (s+totalStats)/K>=goalStats:
-        caseAvgStats.append((s+totalStats)/K);
+    tempList = list(probStats[i])
+    for k in range(0,len(alreadyFilled)):
+        tempList.append(alreadyFilled[k])
+    finalStat = calStat(tempList)
+    if finalStat>=goalStats:
+        caseAvgStats.append(finalStat);
         totalPrice.append(p);
         caseStats.append(probStats[i])
         casePrice.append(probPrice[i])
+    tempList.clear()
 
 # printing result
 
 print("Total price is " + str(min(totalPrice)))
 print(caseStats[totalPrice.index(min(totalPrice))])
 print(caseAvgStats[totalPrice.index(min(totalPrice))])
-
-
-#print result
-print(totalStats)
