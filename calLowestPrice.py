2b5e03e46b8b4268068c9cb075142ffa6fb1eb93
Born2bATM <kachamaslove25@gmail.com>
2023-10-31 00:27:02 +0700

    Chatgpt refactor (delete unnecessary loop

--- calLowestPrice.py
+++ calLowestPrice.py
@@ -1,62 +1,42 @@
-
-
-# Python3 code to demonstrate working of
-# K Dice Combinations
-# Using list comprehension + product()
 from itertools import product
 import math
- 
-# initializing K (playersNum)
+import timeit
+
+start = timeit.default_timer()
 playersNum = 11
 goalStats = 86
 
 def totalStats(listOfStats):
-    totalStats=0
-    for i in range(0,len(listOfStats)):
-        totalStats+=listOfStats[i]
-    return totalStats
+    return sum(listOfStats)
 
-def calStat(listOfStats):
-    totalStat = totalStats(listOfStats)
-    AvgStat = totalStat/playersNum
-    totalBoostPoints=0
-    for i in range(0,len(listOfStats)):
-        if(listOfStats[i]>AvgStat):
-            totalBoostPoints += (listOfStats[i]-AvgStat)
-    return math.floor(round(totalStat+totalBoostPoints)/11)
+def calStat(listOfStats, playersNum):
+    totalStat = sum(listOfStats)
+    AvgStat = totalStat / playersNum
+    totalBoostPoints = sum(max(0, stat - AvgStat) for stat in listOfStats)
+    return math.floor(round(totalStat + totalBoostPoints) / playersNum)
 
+stats = [[84, 85, 86, 87] for _ in range(playersNum)]
+price = [[1200, 6000, 11000, 16500] for _ in range(playersNum)]
 
-# Add up your initial, unrounded average player rating to the average points boost.
-# using list comprehension to formulate elements
-
-# stats = [list([82,83,84,85,86,87]) for _ in range(playersNum)]
-stats = [list([84,85,86,87]) for _ in range(playersNum)]
-
-# price = [list([600,900,2100,6250,11100,17000]) for _ in range(playersNum)]
-price = [list([1200,6000,11000,16500]) for _ in range(playersNum)]
- 
-# using product() to get Combinations
 probStats = list(product(*stats))
 probPrice = list(product(*price))
-caseStats = product()
+
+caseStats = []
 caseAvgStats = 0
-currentMinPrice = 999999
+currentMinPrice = float('inf')
 
-for i in range(0,len(probPrice)):
-    p = 0
-    s = 0
-    for j in range(0,playersNum):
-        p+=probPrice[i][j]
-    finalStat = calStat(probStats[i])
-    if finalStat>=goalStats:
-        if currentMinPrice > p:
-            currentMinPrice = p
-            caseAvgStats = finalStat
-            caseStats = probStats[i]
-        
+for i in range(len(probPrice)):
+    p = sum(probPrice[i])
+    finalStat = calStat(probStats[i], playersNum)
+    
+    if finalStat >= goalStats and currentMinPrice > p:
+        currentMinPrice = p
+        caseAvgStats = finalStat
+        caseStats = probStats[i]
 
-# printing result
-
-print("Total price is " + str(currentMinPrice))
+print("Total price is", currentMinPrice)
 print(caseStats)
 print(caseAvgStats)
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
